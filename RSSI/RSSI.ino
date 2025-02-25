496c377da4215e196029256c65cb501d9bfd3b50
Misara Ahmed <84655733+Misara-Ahmed@users.noreply.github.com>
2025-02-25 19:06:01 +0200

    update rssi & server

--- RSSI/RSSI.ino
+++ RSSI/RSSI.ino
@@ -1,32 +1,3 @@
-// #include <WiFi.h>
-
-// // Replace with your network credentials (STATION)
-// const char* ssid = "Ahmed Said";
-// const char* password = "missarahmed@246";
-
-// void initWiFi() {
-//   WiFi.mode(WIFI_STA);
-//   WiFi.begin(ssid, password);
-//   Serial.print("Connecting to WiFi ..");
-//   while (WiFi.status() != WL_CONNECTED) {
-//     Serial.print('.');
-//     delay(1000);
-//   }
-//   Serial.println(WiFi.localIP());
-// }
-
-// void setup() {
-//   Serial.begin(115200);
-//   initWiFi();
-// }
-
-// void loop()
-// {
-//   Serial.print("RRSI: ");
-//   Serial.println(WiFi.RSSI());
-//   delay(500);
-// }
-
 #include <WiFi.h>
 #include <WiFiClientSecure.h>
 #include <PubSubClient.h>
@@ -42,7 +13,7 @@ const char* mqtt_username = "Misara-Ahmed";
 const char* mqtt_password = "Miso-229";
 
 // MQTT topic to publish in
-const char* clientID = "ESP32"
+const char* clientID = "ESP32";
 const char* topic = "esp32/rssi";
 
 // Create client instances
@@ -51,6 +22,57 @@ PubSubClient mqttClient(wifiClient);
 
 // Variables for timing
 long previous_time = 0;
+
+void setup()
+{
+  Serial.begin(115200);
+
+  initWiFi();
+
+  // Initialize secure WiFiClient
+  wifiClient.setInsecure(); // Use this only for testing, it allows connecting without a root certificate
+  
+  setupMQTT();
+}
+
+void loop()
+{
+  if (!mqttClient.connected())
+  {
+    reconnect();
+  }
+  mqttClient.loop();
+
+  // Publish every 500 milliseconds (0.5 sec)
+  long now = millis();
+  if (now - previous_time > 500)
+  {
+    previous_time = now;
+    int rssi = WiFi.RSSI();
+
+    // Convert the RSSI value to a string
+    String rssi_str = String(rssi);
+    
+    // Publish the sensor value to the MQTT topic
+    Serial.print("RSSI Value: ");
+    Serial.println(rssi_str);
+    mqttClient.publish(topic, rssi_str.c_str());
+  }
+}
+
+void initWiFi()
+{
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print('.');
+    delay(500);
+  }
+  Serial.println("");
+  Serial.println("Connected Successfully");
+}
 
 void setupMQTT()
 {
@@ -75,48 +97,5 @@ void reconnect()
       Serial.println(" try again in 5 seconds");
       delay(5000);
     }
-  }
-}
-
-void setup()
-{
-  Serial.begin(115200);
-  
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("Connected to Wi-Fi");
-
-  // Initialize secure WiFiClient
-  wifiClient.setInsecure(); // Use this only for testing, it allows connecting without a root certificate
-  
-  setupMQTT();
-}
-
-void loop()
-{
-  if (!mqttClient.connected()) {
-    reconnect();
-  }
-  mqttClient.loop();
-
-  long now = millis();
-  if (now - previous_time > 1000) { // Publish every 10 seconds
-    previous_time = now;
-
-    // Read the IR sensor value
-    int ir_sensor_value = analogRead(ir_sensor_pin);
-    int distance = map(ir_sensor_value, 0, 4095, 0, 30);
-    
-    // Convert the value to a string
-    String ir_value_str = String(distance);
-    
-    // Publish the sensor value to the MQTT topic
-    Serial.print("IR Sensor Value: ");
-    Serial.println(ir_value_str);
-    mqttClient.publish(topic_publish_ir, ir_value_str.c_str());
   }
 }
